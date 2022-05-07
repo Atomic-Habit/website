429cd1b50008f48cd0c4c7d6a4a0e2d3b7827ab3
Joris Delorme <joris.delorme38@gmail.com>
2022-05-07 18:06:29 +0200

    warning fix

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -8,13 +8,13 @@ const Navbar = () => {
       <div className="flex h-[40px] absolute w-full z-10 mt-10 sm:px-20 px-10 items-center justify-between">
         <img className='w-[40px]' src={logo} alt="logo" />
         <ul className='sm:flex hidden'>
-          <li className='px-5'><a href="">Acceuil</a></li>
-          <li className='px-5'><a href="">Application</a></li>
-          <li className='px-5'><a href=""></a>Prix</li>
-          <li className='px-5'><a href="">Contact</a></li>
+          <li className='px-5'><a href="/">Acceuil</a></li>
+          <li className='px-5'><a href="/">Application</a></li>
+          <li className='px-5'><a href="/">Prix</a></li>
+          <li className='px-5'><a href="/">Contact</a></li>
         </ul>
         <div className="rounded-10 bg-[#fffff]">
-          <a className='px-5 py-5 bg-[#fffff]' href="#">Rejoindre</a>
+          <a className='px-5 py-5 bg-[#fffff]' href="/">Rejoindre</a>
         </div>
       </div>
     </Fade>
